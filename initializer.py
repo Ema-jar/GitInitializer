4f3e1826ba8f5e87a8e21a31efd0565891ae2fbc
Ema-jar <emanuele.giarlini@gmail.com>
2017-05-14 19:58:29 +0200

    fixed initial script

--- initializer.py
+++ initializer.py
@@ -10,22 +10,26 @@ import time
 BASE_PATH = sys.argv[2]
 BASE_GIT = 'git -C ' + BASE_PATH
 
-# Commits all using git commit command
+# Adds all and commits all using git commit command
 def commit():
+    subprocess.Popen(BASE_GIT + ' add . ', shell=True)
     subprocess.Popen(BASE_GIT + ' commit -m "date: ' + str(time.time()) + '"', shell=True)
     return
 
 # Append a given string to a file
 def update_file(filename, string_to_append):
-    subprocess.Popen('echo "[' + str(time.time()) + '] -> ' + string_to_append + '" >> ' + filename, shell=True)
+    subprocess.Popen('echo "[' + str(time.time()) + '] -> ' + string_to_append + '" >> ' + BASE_PATH + '/' + filename, shell=True)
     return
 
 # Creates an empty folder (if it doesn't exist) and initialize a git repo in it
-def init_git_repo(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
+def init_git_repo():
+    if not os.path.exists(BASE_PATH):
+        os.makedirs(BASE_PATH)
     
     subprocess.Popen(BASE_GIT + ' init ', shell=True)
+    update_file('master_file.txt', 'First commit on master')
+    commit()
+
     return
 
 # Resets the branch on master
@@ -45,28 +49,33 @@ def checkout(branch_name):
 
 command = sys.argv[1]
 
-init_git_repo(BASE_GIT)
+
 
 # initializer.py random ../path 3 4
 if command == 'random':
     n_of_branches = int(sys.argv[3])
     max_n_of_commits = int(sys.argv[4])
 
-    print 'Creating: ', n_of_branches, ' branches.'
-    print 'Max number of commits on each branch: ', max_n_of_commits
+    init_git_repo()
+
 
     for i in range(1, n_of_branches + 1):
+
+        #create new branch
         branch_name = 'new_branch_' + str(i)
         create_branch(branch_name)
+
+        #checkout on the created branh
         checkout(branch_name)
 
+        #create a new file
         filename = 'file_in_branch_' + str(i) + '.txt'
         random_string = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
         update_file(filename, random_string)
 
+        # commit that file
         commit()
 
+        #come back to master
         reset()
 
-print 'Inviati ' , len(sys.argv), ' arguments'
-print 'Arguments: ', str(sys.argv)
