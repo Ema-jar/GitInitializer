9e86159492caa10ab0a2f5b2cde61564efea32e4
Emanuele Giarlini <emanuele.giarlini@contactlab.com>
2017-05-19 13:57:13 +0200

    working on check_parameters

--- check_params.py
+++ check_params.py
@@ -1,1 +1,24 @@
 # Contains all the function used to check the inputs from shell
+
+
+def check_params(params):
+
+    command = params[0]
+    params.pop(0)
+
+    if command == 'simple':
+        check_simple(params)
+
+    if command == 'random':
+        check_random(command)
+
+    return
+
+
+def check_simple(params):
+    return
+
+
+def check_random(params):
+    return
+
